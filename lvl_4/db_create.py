4be5ac473f07e39f6c7270f19ef45ec89b75f37b
VladPavlukhin <vboxhome@mail.ru>
2023-05-05 16:37:55 +0300

    added lvl_4

---
+++ lvl_4/db_create.py
@@ -0,0 +1,79 @@
+import sqlite3
+
+global conn
+global cur
+
+
+def create_table(table_name, fields):
+    sql = 'create table if not exists ' + table_name + '(' + fields + ')'
+    cur.execute(sql)
+    conn.commit()
+
+
+def del_table(table_name):
+    sql = 'drop table ' + table_name
+    cur.execute(sql)
+    conn.commit()
+
+
+def remake(table_name):
+    del_table(table_name)
+    create_table(table_name)
+    # insert_record(1)
+    conn.commit()
+
+
+def insert_record(table_name, fields, data):
+    sql = 'insert into ' + table_name + ' (' + fields + ') values (?, ?, ?);'
+    cur.execute(sql, data)
+    print('Inserted record ', data)
+    conn.commit()
+
+
+def fill_table_students():
+    create_table('Students', 'School_Id INT PRIMARY KEY, Student_Id INT, Student_Name TEXT')
+    fields_students = 'School_Id, Student_Id, Student_Name'
+    data_students = [[1, 201, "Иван"],
+                     [2, 202, "Петр"],
+                     [3, 203, "Анастасия"],
+                     [4, 204, "Игорь"]]
+
+    for d in range(len(dataStudents)):
+        insert_record('Students', fieldsStudents, dataStudents[d])
+
+
+def close_conn():
+    cur.close()
+    conn.close()
+
+
+db = 'teachers.db'
+conn = sqlite3.connect(db)
+cur = conn.cursor()
+
+
+get_student_id = (input('Введите Id студента: '))
+query = """SELECT s.Student_Id,
+                  s.Student_Name,
+                  l.School_Id,
+                  l.School_Name
+           FROM Students s, School l
+           WHERE Student_Id=? AND s.School_Id = l.School_Id"""
+
+cur.execute(query, (get_student_id,))
+student = cur.fetchall()
+
+try:
+    print("ID Студента: " + str(student[0][0]) + "\n" 
+      "Имя студента: " + student[0][1] + "\n"
+      "ID школы: " + str(student[0][2]) + "\n"
+      "Название школы: " + student[0][3] + "\n")
+except TypeError:
+    print("Нет такого студента")
+
+close_conn()
+
+
+# del_table('Students')
+# remake('Students')
+# fill_table_students
